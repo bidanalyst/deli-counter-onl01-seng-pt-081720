42dd46d83a5ccbe1408cf10345ba72b66e4a6252
Jeff Michelot <jeffmichelot@gmail.com>
2020-08-31 10:59:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+# define a method-line 
+# line == 0 then puts "the line is currently empty" 
+# 
+
+def line(array)
+  if array.length == 0
+  puts "The line is currently empty."
+end
